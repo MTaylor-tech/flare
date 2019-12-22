c1d1bd457105e91d4f496a5e4863df3fad2bb316
Matthew Taylor <majikpig@gmail.com>
2019-12-22 10:31:08 -0500

    fixed settings

--- flare/settings.py
+++ flare/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,7 +32,7 @@ DEBUG = True
 X_FRAME_OPTIONS = 'DENY'
 
 ALLOWED_HOSTS = ['127.0.0.1']
-GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
 
 
 
